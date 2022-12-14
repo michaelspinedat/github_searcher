c779072f44b112eb6b5b616dbc9d2ee2a474de9f
Michael Steven Pineda Torres <stevenpnd@gmail.com>
2022-12-13 21:32:06 -0500

    Set up project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Container } from '@mui/material'
+import React from 'react'
+
+function App() {
+
+  return (
+    <Container>
+      <h1>Hola mundo</h1>
+    </Container>
+  )
+}
+
+export default App
